08703a2c4f0c044b79ce6498da42464b2df13535
James Stuart <james@stuart.name>
2015-07-01 17:28:42 -0400

    add repo to ambari

--- manifests/init.pp
+++ manifests/init.pp
@@ -10,6 +10,22 @@
 #
 # Sample Usage:
 #
-class bigmatch {
-
+class bigmatch (
+  $bigmatch_repo_uri = undef,
+  $bigmatch_repo_gpgcheck = true,
+  $bigmatch_repo_sslverify = true,
+) {
+  validate_string($bigmatch_repo_uri)
+  validate_bool($bigmatch_repo_gpgcheck)
+  validate_bool($bigmatch_repo_sslverify)
+  
+  # Add the BigMatch repo to Ambari
+  iop::ambari::repo { 'bigmatch':
+    repoid    => 'BIGMATCH-11.4',
+    reponame  => 'BIGMATCH',
+    baseurl   => $bigmatch::bigmatch_repo_uri,
+    gpgcheck  => $bigmatch::bigmatch_repo_gpgcheck,
+    sslverify => $bigmatch::bigmatch_repo_sslverify,
+    order     => '30'
+  }
 }
