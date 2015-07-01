5865e7cb739a8b44c05e445e72386791cfd15f10
James Stuart <james@stuart.name>
2015-07-01 17:33:35 -0400

    docs

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,14 +1,44 @@
 # Class: bigmatch
 #
-# This module manages bigmatch
+# This module manages the deployment of IBM Big Match for Hadoop on IBM Open Platform with Apache Hadoop.
 #
-# Parameters: none
+# Notes:
+#  * This is designed specifically for Enterprise Linux 6 based systems. Others will not work.
+#  * IBM Big Match is commercial software.  It is NOT included as part of this module.
+#  * You must obtain the software yourself, agreeing to any and all terms required 
+#    by IBM.  See [Installing InfoSphere Big Match for Hadoop](http://www-01.ibm.com/support/knowledgecenter/SSWSR9_11.4.0/com.ibm.swg.im.mdmhs.pmebi_install.doc/topics/installing_pme_bi.html)
+#  * This module does not support the removal of software.
+#
+# Parameters:
+#
+# $bigmatch_repo_uri::             The URI of the Yum repository containing
+#                                  the Big Match software.  
+#                                  Required, no default
+#                                  type:string
+#
+# $bigmatch_repo_gpgcheck::        Flag that instructs Yum to check package
+#                                  signatures prior to installation. Setting
+#                                  this to false is almost always a bad idea.
+#                                  Defaults to true.
+#                                  type:boolean
+#
+# $bigmatch_repo_sslverify::       Flag that instructs Yum to check the validity
+#                                  of the certificate provided by the remote
+#                                  repository (e.g. https server certificate).
+#                                  Setting this to false is almost always a
+#                                  bad idea.
+#                                  Defaults to true.
+#                                  type:boolean
 #
 # Actions:
 #
 # Requires: see Modulefile
 #
 # Sample Usage:
+#
+# * Simple usage
+#
+#     include bigmatch
 #
 class bigmatch (
   $bigmatch_repo_uri = undef,
